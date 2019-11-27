195cde700c511c7142dc4f593c9a17d8c2cec4c2
PeiYumeng <457492875@qq.com>
2019-11-27 11:00:21 +0800

    message

---
+++ 3. 前端代码/chuanda/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppTab from './AppTab'
+function App() {
+  return (
+    <div className="App">
+     <AppTab />
+    </div>
+  );
+}
+
+export default App;
