448fa829990e60d9c84bc3970bb007a4036127ba
Lucas-Pan-Wang <1150473958@qq.com>
2019-12-04 16:23:16 +0800

    login improve

--- 3. 前端代码/yishe/src/wear/Wear.js
+++ 3. 前端代码/yishe/src/wear/Wear.js
@@ -19,6 +19,7 @@ import buganxingqu from '../images/不感兴趣_44.png'
 import jubao from '../images/举报.png'
 import xiaoren from '../images/小人.png'
 import beijing from '../images/雨.jpg'
+import {Consumer} from '../context'
 
 const data = Array.from(new Array(5)).map((_val, i) => ({
     icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
@@ -92,7 +93,8 @@ export default class Wear extends Component {
 
     render() {
         return (
-            <div className="body" style={{width:'100%',height:'100%',background:"url("+beijing+")"}}>
+          <Consumer>
+            {(data)=>{return <div className="body" style={{width:'100%',height:'100%',background:"url("+beijing+")"}}>
               {/* 头 */}
                 <NavBar style={{backgroundColor:'#fc9d9a',color:'white'}}
                 >穿搭</NavBar>
@@ -145,9 +147,10 @@ export default class Wear extends Component {
                       )}
                     />
                     <hr />
-                    <p onClick={this.click_unShare}>取消</p>
+                    <p onClick={()=>{this.click_unShare(); console.log('data.userId:'+data.userId)}}>取消</p>
                   </div>
-            </div>
+            </div>}}
+            </Consumer>
         );
     }
 }
