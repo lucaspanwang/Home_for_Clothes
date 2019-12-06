80f928f782319e45f19809f6c4189f1e89613bf9
Lucas-Pan-Wang <1150473958@qq.com>
2019-12-06 10:11:15 +0800

    delete

--- 3. 前端代码/Yishe_final/config-overrides.js
+++
@@ -1,6 +0,0 @@
-module.exports = function override(config, env) {
-    
-    config.output.publicPath = './';
-   
-    return config;
-  };
--- 3. 前端代码/Yishe_final/src12.3/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import 'antd/dist/antd.css';
-import 'antd-mobile/dist/antd-mobile.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
