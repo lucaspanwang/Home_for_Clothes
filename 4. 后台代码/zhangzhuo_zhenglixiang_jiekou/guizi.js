479e572a8b37f4d1f00490e4d00f1b2c56a8c5fa
zhangzhuozhang <2028251635@qq.com>
2019-12-11 17:02:40 +0800

    add

--- 4. 后台代码/zhangzhuo_zhenglixiang_jiekou/guizi.js
+++ 4. 后台代码/zhangzhuo_zhenglixiang_jiekou/guizi.js
@@ -73,21 +73,49 @@ server.on('request',(req,res)=>{
                     var cloId=JSON.stringify(cloId1);
                     var weizhi=JSON.stringify(JSON.parse(obj).weizhi[0]);
                     var yanse = JSON.stringify(JSON.parse(obj).yanse[0]);
-                    var cloSmallPic='';
+                    var cloSmallPic;
                     var cloPic=JSON.stringify('我的/images/'+cloId1+filesType);
+                    console.log(yanse=='"黑色"')
+                    if(zhonglei=='"裤子运动裤"'){
+                        cloSmallPic=JSON.stringify('我的/images/yundongku.png');
+                    }else if(zhonglei=='"裤子牛仔裤"'){
+                        cloSmallPic=JSON.stringify('我的/images/kuku.png');
+                    }else if(zhonglei=='"裤子短裤"'){
+                        cloSmallPic=JSON.stringify('我的/images/duanku1.png');
+                    }else if(zhonglei=='"裙子短裙"'){
+                        cloSmallPic=JSON.stringify('我的/images/duanqun.png')
+                    }else if(zhonglei=='"裙子长裙"'){
+                        cloSmallPic=JSON.stringify('我的/images/changchun.png')
+                    }else if(zhonglei=='"裙子吊带裙"'){
+                        cloSmallPic=JSON.stringify('我的/images/heiqun.png')
+                    }else if(zhonglei=='"裙子保守裙"'){
+                        if(yanse=='')
+                        cloSmallPic=JSON.stringify('我的/images/duanqun.png')
+                    }else if(zhonglei=='"上衣毛衣"'){
+                        cloSmallPic=JSON.stringify('我的/images/maoyi.png')
+                    }else if(zhonglei=='"上衣运动衣"'){
+                        if(yanse=='')
+                        cloSmallPic=JSON.stringify('我的/images/duanqun.png')
+                    }else if(zhonglei=='"上衣卫衣"'){
+                        cloSmallPic=JSON.stringify('我的/images/changshangyi.png')
+                    }else if(zhonglei=='"外套薄外套"'){
+                        cloSmallPic=JSON.stringify('我的/images/chenshantao.png')
+                    }else if(zhonglei=='"外套厚外套"'){
+                        cloSmallPic=JSON.stringify('我的/images/waitao.png')
+                    }
 
-                    con.query(`insert into clothing values(${cloId},${userid},${zhonglei},${weizhi},${yanse},${cloPic},'dkls')`);
+                    // con.query(`insert into clothing values(${cloId},${userid},${zhonglei},${weizhi},${yanse},${cloPic},${cloSmallPic})`);
 
-                    var path='../我的/images/'+cloId1+filesType;
-                    console.log(path);
-                    var dataBuffer=new Buffer(base64,'base64');
-                    fs.writeFile(path,dataBuffer,function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            console.log('写入成功');
-                        }
-                    })
+                //     var path='../我的/images/'+cloId1+filesType;
+                //     console.log(path);
+                //     var dataBuffer=new Buffer(base64,'base64');
+                //     fs.writeFile(path,dataBuffer,function(err){
+                //         if(err){
+                //             console.log(err);
+                //         }else{
+                //             console.log('写入成功');
+                //         }
+                //     })
                 })
             }
         })
