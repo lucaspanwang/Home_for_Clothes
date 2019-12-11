082b95fed81247cb62a3a2f16e2ed4b0b93ff321
zhangzhuozhang <2028251635@qq.com>
2019-12-11 14:57:10 +0800

    add

--- 4. 后台代码/zhangzhuo_zhenglixiang_jiekou/guizi.js
+++ 4. 后台代码/zhangzhuo_zhenglixiang_jiekou/guizi.js
@@ -59,21 +59,35 @@ server.on('request',(req,res)=>{
                     if(JSON.parse(obj).base64[0]!=undefined){
                         var base64=JSON.parse(obj).base64[0].url.split(',')[1];
                     }
-                    var zhonglei=JSON.stringify(JSON.parse(obj).zhonglei[0]);
+
+                    var zhonglei1='';
+                    JSON.parse(obj).zhonglei.forEach(element => {
+                        zhonglei1+=element;
+                    });
+                    var zhonglei=JSON.stringify(zhonglei1);
+                    var filesType=JSON.parse(obj).filesType[0];
+                    console.log(filesType);
+                    var userid=JSON.stringify(JSON.parse(obj).userid);
+                    var data=new Date();
+                    var cloId1=''+data.getFullYear()+data.getMonth()+data.getDate()+data.getHours()+data.getMinutes()+data.getSeconds();
+                    var cloId=JSON.stringify(cloId1);
                     var weizhi=JSON.stringify(JSON.parse(obj).weizhi[0]);
                     var yanse = JSON.stringify(JSON.parse(obj).yanse[0]);
-                    con.query(`insert into clothing(cloId,userId,cloKind,cloPlace,cloColor,cloPic,cloSmallPic)
-                    values('191',${user},${zhonglei},${weizhi},${yanse},'....',',,,,,')`)
-                    // var path='../try/one.jpg';
-                    // var dataBuffer=new Buffer(base64,'base64');
-                    // fs.writeFile(path,dataBuffer,function(err){
-                    //     if(err){
-                    //         console.log(err);
-                    //     }else{
-                    //         console.log('写入成功');
-                    //     }
-                    // })
-                    // console.log(random(4, {letters: false}));
+                    var cloSmallPic='';
+                    var cloPic=JSON.stringify('我的/images/'+cloId1+filesType);
+
+                    con.query(`insert into clothing values(${cloId},${userid},${zhonglei},${weizhi},${yanse},${cloPic},'dkls')`);
+
+                    var path='../我的/images/'+cloId1+filesType;
+                    console.log(path);
+                    var dataBuffer=new Buffer(base64,'base64');
+                    fs.writeFile(path,dataBuffer,function(err){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            console.log('写入成功');
+                        }
+                    })
                 })
             }
         })
