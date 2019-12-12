de17914e5555ca4390d28d4b9cff46d2366f003b
Lucas-Pan-Wang <1150473958@qq.com>
2019-12-12 08:08:04 +0800

    login edit

---
+++ 3. 前端代码/yishe/src/login/CitySelector.js
@@ -0,0 +1,94 @@
+import {Picker} from 'antd-mobile'
+import React from 'react'
+import {Consumer} from '../context'
+
+const CustomChildren = props => (
+    <div
+        onClick={props.onClick}
+        style={{ backgroundColor: '#fff', paddingLeft: 15, border:'solid 1px grey',
+        borderRadius:'6px', marginBottom:'10%'}}
+    >
+        <div className="test" style={{ fontSize:'18px', display: 'flex', height: '42px', lineHeight: '42px',position:'relative',borderBottom:0, color:'grey'}}>
+            <div style={{ flex: 1, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{props.children}</div>
+            <div style={{ textAlign: 'right', color: 'grey', marginRight: 15 }}>{props.extra}</div>
+        </div>
+    </div>
+);
+ 
+export default class CitySelector extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            pickerValue: [],
+            userCity:''
+        };
+        this.changeCity = this.changeCity.bind(this);
+    }
+
+    changeCity(){
+        var code0 = this.state.pickerValue[0];
+        var code1 = this.state.pickerValue[1];
+        let districtData = require('./location.json');
+        for(var k in districtData){
+            if(districtData[k].code==code0){
+                for(var m in districtData[k].cities){
+                    if (districtData[k].cities[m].code==code1){
+                        var city = districtData[k].cities[m].name;
+                        this.setState({userCity:city});
+                    }
+                }
+                }
+            }
+        }
+ 
+    render(){
+        let antdDistrict =[];
+        let districtData = require('./location.json');
+        Object.keys(districtData).forEach((index)=>{
+            let itemLevel1 ={};
+            let itemLevel2 ={};
+            itemLevel1.value = districtData[index].code;
+            itemLevel1.label = districtData[index].name;
+            itemLevel1.children = [];
+            let data = districtData[index].cities;
+            Object.keys(data).forEach((index)=>{
+                itemLevel2.value = data[index].code;
+                itemLevel2.label = data[index].name;
+                itemLevel2.children = [];
+                let data2 = data[index].districts;
+                let itemLevel3 ={};
+                itemLevel3.children = [];
+                Object.keys(data2).forEach((index)=>{
+                    itemLevel3.value = index;
+                    itemLevel3.label = data2[index];
+                    itemLevel2.children.push(itemLevel3);
+                    itemLevel3 ={};
+                });
+                itemLevel1.children.push(itemLevel2);
+                itemLevel2 ={};
+            });
+            antdDistrict.push(itemLevel1)
+        });
+        // console.log(antdDistrict);
+        return (
+            <Consumer>
+                {
+                (data)=>{return <div>
+                    <Picker
+                        title="选择地区"
+                        // extra="请选择(可选)"
+                        data={antdDistrict}
+                        value={this.state.pickerValue}
+                        onChange={v => this.setState({ pickerValue: v })}
+                        onOk={(v) => {this.setState({ pickerValue: v })}}
+                        onPickerChange={(v) => {this.setState({ pickerValue: v });this.changeCity();data.userCity=this.state.userCity;console.log(data.userCity)}}
+                        onClick={()=>{console.log('xx')}}
+                    >
+                        <CustomChildren>请选择城市</CustomChildren>
+                    </Picker>
+                </div>}
+                }
+            </Consumer>
+        )
+    }
+  }
--- 3. 前端代码/yishe/src/login/Register.js
+++ 3. 前端代码/yishe/src/login/Register.js
@@ -1,11 +1,18 @@
 import React, {Component} from "react";
 import fanhui from '../images/返回 (1).png';
-import { NavBar, WingBlank, Flex, InputItem, Button} from 'antd-mobile';
+import { NavBar, WingBlank, Flex, InputItem, Button, Picker} from 'antd-mobile';
 import { Link, Route, HashRouter as Router } from 'react-router-dom';
 import camera from '../images/camera.png';
 import phone from '../images/phone.png';
 import doc from '../images/doc.png';
 import lock from '../images/lock_grey.png';
+import userName from '../images/userName.png';
+import male from '../images/male.png';
+import female from '../images/female.png';
+import city from '../images/city.png';
+import CitySelector from './CitySelector';
+import {Consumer} from '../context';
+
 import './login.css';
 
 export default class Register extends Component{
@@ -15,10 +22,24 @@ export default class Register extends Component{
             url:'http://47.98.163.228:8082/register',
             userPho:'',
             userPwd:'',
+            userName:'',
+            userSex:'',
+            userCity:''
         }
+        this.nextStep = this.nextStep.bind(this);
         this.postData = this.postData.bind(this);
+        this.goLogin = this.goLogin.bind(this);
     }
 
+    nextStep(){
+        document.getElementById('step1').style.display='none';
+        document.getElementById('step2').style.display='block';
+    }
+
+    goLogin(){
+        document.getElementById('judgeR').style.display='block';
+    }
+    
     postData(){
         fetch("http://47.98.163.228:8082/register", {
         method: 'post', 
@@ -28,10 +49,21 @@ export default class Register extends Component{
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: JSON.stringify({userPho:this.state.userPho, userPwd:this.state.userPwd})
+        body: JSON.stringify({userPho:this.state.userPho, userPwd:this.state.userPwd, userName:this.state.userName, userSex:this.state.userSex, userCity:this.state.userCity})
       })
     }
     
+    componentDidUpdate(){
+        if(this.state.userSex=='男'){
+            document.getElementById('malePic').style.display='inline-block';
+            document.getElementById('femalePic').style.display='none';
+        }
+        if(this.state.userSex=='女'){
+            document.getElementById('malePic').style.display='none';
+            document.getElementById('femalePic').style.display='inline-block';
+        }
+    }
+
     // hrefChange(str){
     //     var h=window.location.href;
     //     var index = h.lastIndexOf("\/");  
@@ -39,34 +71,61 @@ export default class Register extends Component{
     // }
 
     render(){
-        return <div style={{width:'100%',height:'100%', position:'relative', backgroundColor:'white'}}>
-        <NavBar 
-                style={{backgroundColor:'#fc9d9a',color:'white'}}
-                leftContent={[
-                    // <a onClick={()=>{this.hrefChange('login')}}><img src={fanhui} style={{width:'25%'}} key="fan"/></a>
-                    <Link to="/login"><img src={fanhui} style={{width:'25%'}} key="fan"/></Link>
-                ]}
-                >注册
-        </NavBar>
-        <Flex direction="column" justify="center" align="center" style={{paddingTop:'5%'}}>
-            <div id="camera_div">
-                <img src={camera} width="50%" style={{position:'absolute', left:'24%', top:'15%'}}/>
-                <p style={{position:'absolute', top:'62%', left:'21%', color:'#a8a8a8'}}>添加头像</p>
+        return(
+            <Consumer>
+                {
+                (data)=>{return <div style={{width:'100%',height:'100%', position:'relative', backgroundColor:'white'}}>
+                <NavBar 
+                        style={{backgroundColor:'#fc9d9a',color:'white'}}
+                        leftContent={[
+                            // <a onClick={()=>{this.hrefChange('login')}}><img src={fanhui} style={{width:'25%'}} key="fan"/></a>
+                            <Link to="/login"><img src={fanhui} style={{width:'25%'}} key="fan"/></Link>
+                        ]}
+                        >注册
+                </NavBar>
+                <Flex id='judgeR' class="judge2" direction="column" justify="center" align="center" style={{display:'none'}}>
+                        <p>&nbsp;&nbsp;注册成功！</p>
+                        <Link to="/login"><Button onClick={this.postData} style={{marginTop:'20%', marginLeft:'15%', color:'white', fontSize:'90%', fontWeight:'bold', width:'70%', height:'20%', backgroundColor:'rgb(36,217,238)', border:'solid 1px blue'}}>去 登 录</Button></Link>
+                </Flex>
+                <Flex direction="column" justify="center" align="center" style={{paddingTop:'5%'}}>
+                    <div id="camera_div">
+                        <img src={camera} width="50%" style={{position:'absolute', left:'24%', top:'15%'}}/>
+                        <p style={{position:'absolute', top:'62%', left:'21%', color:'#a8a8a8'}}>添加头像</p>
+                    </div>
+                    <div id='register_info'>
+                        <form id='step1'>
+                        <ul id='register_input1'>
+                            <li><InputItem className='register_input2' placeholder='用户名' maxLength='7' onChange={(e)=>this.setState({userName:e})}/></li>
+                            <CitySelector />
+                            <li>
+                                <img id='femalePic' src={female} width="14%" style={{marginLeft:'15%'}}/><img id='malePic' src={male} width="14%" style={{marginLeft:'15%', display:'none'}}/><p style={{marginLeft:'10%', display:'inline-block', fontSize:'18px', color:'#a8a8a8'}}>性别</p>
+                                <select id="sexSelect" name="sex" onChange={()=>{var sex = document.getElementById('sexSelect'); this.setState({userSex:sex.value})}}>
+                                    <option value="女" selected>女</option>
+                                    <option value="男">男</option>
+                                </select>  
+                            </li>
+                        </ul>
+                            <Flex direction="column" justify="center" align="center">
+                                <Button onClick={()=>{this.nextStep();this.setState({userCity: data.userCity})}} style={{textAlign:'center', backgroundColor:'#fc9d9a', color:'white', width:'80%', marginTop:'5%'}}>下 一 步</Button>
+                            </Flex>
+                        </form>
+                        <form id='step2' style={{display:'none'}}>
+                        <ul id='register_input2'>
+                            <li><img src={phone} width="14%"/><InputItem className='register_input' placeholder='手机号码' type='phone' onChange={(e)=>this.setState({userPho:e})}/></li>
+                            {/* <li style={{position:'relative'}}><img src={doc} width="14%"/><InputItem className='register_input' placeholder='验证码' type='number' maxLength='6'/><Button style={{position:'absolute', right:'0', top:'0',  backgroundColor:'#fc9d9a', width:'35%', color:'white'}}>发送验证码</Button></li> */}
+                            <li><img src={lock} width="14%"/><InputItem className='register_input' placeholder='密码(6-16位数字字母组合)' type='password' maxLength='16' onChange={(e)=>this.setState({userPwd:e})}/></li>
+                        </ul>
+                            <Flex direction="column" justify="center" align="center">
+                                <Button onClick={this.goLogin} style={{textAlign:'center', backgroundColor:'#fc9d9a', color:'white', width:'80%', marginTop:'5%'}}>立 即 注 册</Button>
+                                <p style={{marginTop:'8%', }}>注册代表您同意<a style={{color:'#fc9d9a'}}>《用户协议》</a></p>
+                            </Flex>
+                        </form>
+                    </div>
+                </Flex>
             </div>
-            <div id='register_info'>
-                <form>
-                <ul id='register_input'>
-                    <li><img src={phone} width="14%"/><InputItem className='register_input' placeholder='手机号码' type='phone' onChange={(e)=>this.setState({userPho:e})}/></li>
-                    {/* <li style={{position:'relative'}}><img src={doc} width="14%"/><InputItem className='register_input' placeholder='验证码' type='number' maxLength='6'/><Button style={{position:'absolute', right:'0', top:'0',  backgroundColor:'#fc9d9a', width:'35%', color:'white'}}>发送验证码</Button></li> */}
-                    <li><img src={lock} width="14%"/><InputItem className='register_input' placeholder='密码(6-16位数字字母组合)' type='password' maxLength='16' onChange={(e)=>this.setState({userPwd:e})}/></li>
-                </ul>
-                    <Flex direction="column" justify="center" align="center">
-                        <Button onClick={this.postData} style={{textAlign:'center', backgroundColor:'#fc9d9a', color:'white', width:'80%', marginTop:'5%'}}>立 即 注 册</Button>
-                        <p style={{marginTop:'8%', }}>注册代表您同意<a style={{color:'#fc9d9a'}}>《用户协议》</a></p>
-                    </Flex>
-                </form>
-            </div>
-        </Flex>
-    </div>
+        }
+    }
+        </Consumer>
+        )
     }
 }
