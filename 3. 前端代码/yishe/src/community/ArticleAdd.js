47e6a16558e9117b5e29a33e37114bd6cea5a56a
lishuangg <1241982864@qq.com>
2019-12-18 14:16:33 +0800

    add

--- 3. 前端代码/yishe/src/community/ArticleAdd.js
+++ 3. 前端代码/yishe/src/community/ArticleAdd.js
@@ -19,22 +19,54 @@ export default class ArticleAdd extends Component {
         if(this.state.value == '' && this.state.cimg == ''){
             return ;
         }else{
+            var today = new Date();
+            var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+            var cimgName = [];
+            var cimg = [];
+            let promise = new Promise(resolve =>{
+                for(var i=0;i<this.state.cimg.length;i++){
+                    cimgName.push('.'+this.state.cimg[i].file.name.split(".")[1]);
+                    lrz(this.state.cimg[i].url, {quality:0.1})
+                    .then((res)=>{
+                        cimg.push(res.base64);
+                        console.log(cimg);
+                        resolve({cimg:cimg,cimgName:cimgName});
+                    });
+                }
+            }).then((value)=>{
+                console.log(value);
+                fetch('http://47.98.163.228:8086/articleAdd',{
+                    method: 'post', 
+                    "Access-Control-Allow-Origin" : "*",
+                    "Access-Control-Allow-Credentials" : true,
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: JSON.stringify({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:value.cimg,cimgName:value.cimgName}) 
+                });
+            })
             // let promise = new Promise(resolve =>{
             //     var today = new Date();
             //     var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
             //     var cimgName = [];
             //     var cimg = [];
-            //     for(var i=0;i<this.state.cimg.length;i++){
-            //         cimgName[i]='.'+this.state.cimg[i].file.name.split(".")[1];
-            //         // console.log(this.state.cimg[i].url);
-            //         lrz(this.state.cimg[i].url, {quality:0.1})
-            //         .then((res)=>{
-            //             // console.log(res.base64);
-            //             cimg[i]=res.base64;
-            //             console.log(cimg[i]);
-            //         });
-            //     }
-            //     resolve({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:cimg,cimgName:cimgName})
+            //     let promise = new Promise(resolve =>{
+            //         for(var i=0;i<this.state.cimg.length;i++){
+            //             cimgName.push('.'+this.state.cimg[i].file.name.split(".")[1]);
+            //             // console.log(this.state.cimg[i].url);
+            //             lrz(this.state.cimg[i].url, {quality:0.1})
+            //             .then((res)=>{
+            //                 // console.log(res.base64);
+            //                 cimg.push(res.base64);
+            //                 console.log(cimg);
+            //                 resolve({cimg:cimg,cimgName:cimgName});
+            //             });
+            //         }
+            //     }).then((value)=>{
+            //         console.log(value);
+            //         resolve({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:value.cimg,cimgName:value.cimgName})
+            //     })
             // }).then(value=>{
             //     fetch('http://47.98.163.228:8086/articleAdd',{
             //         method: 'post', 
@@ -47,81 +79,6 @@ export default class ArticleAdd extends Component {
             //         body: JSON.stringify(value) 
             //     });
             // })
-
-            let promise = new Promise(resolve =>{
-                var today = new Date();
-                var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
-                var cimgName = [];
-                var cimg = [];
-                let promise = new Promise(resolve =>{
-                    for(var i=0;i<this.state.cimg.length;i++){
-                        cimgName.push('.'+this.state.cimg[i].file.name.split(".")[1]);
-                        // console.log(this.state.cimg[i].url);
-                        lrz(this.state.cimg[i].url, {quality:0.1})
-                        .then((res)=>{
-                            // console.log(res.base64);
-                            cimg.push(res.base64);
-                            console.log(cimg);
-                            resolve({cimg:cimg,cimgName:cimgName});
-                        });
-                    }
-                }).then((value)=>{
-                    console.log(value);
-                    resolve({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:value.cimg,cimgName:value.cimgName})
-                })
-                // for(var i=0;i<this.state.cimg.length;i++){
-                //     cimgName[i]='.'+this.state.cimg[i].file.name.split(".")[1];
-                //     // console.log(this.state.cimg[i].url);
-                //     lrz(this.state.cimg[i].url, {quality:0.1})
-                //     .then((res)=>{
-                //         // console.log(res.base64);
-                //         cimg[i]=res.base64;
-                //         console.log(cimg[i]);
-                //     });
-                // }
-                // resolve({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:cimg,cimgName:cimgName})
-            }).then(value=>{
-                fetch('http://47.98.163.228:8086/articleAdd',{
-                    method: 'post', 
-                    "Access-Control-Allow-Origin" : "*",
-                    "Access-Control-Allow-Credentials" : true,
-                    credentials: 'include',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: JSON.stringify(value) 
-                });
-            })
-
-            // var today = new Date();
-            // var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
-            // var cimgName = [];
-            // var cimg = [];
-            // for(var i=0;i<this.state.cimg.length;i++){
-            //     cimgName[i]='.'+this.state.cimg[i].file.name.split(".")[1];
-            //     // console.log(this.state.cimg[i].url);
-            //     lrz(this.state.cimg[i].url, {quality:0.1})
-            //     .then((res)=>{
-            //         // console.log(res.base64);
-            //         cimg[i]=res.base64;
-            //         console.log(cimg[i]);
-            //         // console.log(res.base64);
-            //         // console.log(res.base64Len);
-            //         // console.log(res.file.size);
-            //         // console.log(res.fileLen);
-            //     });
-            // }
-            // // console.log(cimg);
-            // fetch('http://47.98.163.228:8086/articleAdd',{
-            //     method: 'post', 
-            //     "Access-Control-Allow-Origin" : "*",
-            //     "Access-Control-Allow-Credentials" : true,
-            //     credentials: 'include',
-            //     headers: {
-            //         'Content-Type': 'application/x-www-form-urlencoded'
-            //     },
-            //     body: JSON.stringify({userId:this.props.match.params.id,content:this.state.value,time:date,cimg:cimg,cimgName:cimgName}) 
-            // });
         }
         localStorage.setItem('come',1);
     }
@@ -144,8 +101,8 @@ export default class ArticleAdd extends Component {
                     <Link to={"/shequtab/"+this.props.match.params.id}><img src={fanhui} style={{width:'30px'}} key="fan"/></Link>
                 ]}
                 rightContent={[
-                    <p style={{backgroundColor:'#fc9d9a',color:'white',fontSize:'18px'}} onClick={this.onPost}>发布</p>
-                    // <Link to={"/shequtab/"+this.props.match.params.id} style={{backgroundColor:'#fc9d9a',color:'white',fontSize:'18px'}} onClick={this.onPost}>发布</Link>
+                    // <p style={{backgroundColor:'#fc9d9a',color:'white',fontSize:'18px'}} onClick={this.onPost}>发布</p>
+                    <Link to={"/shequtab/"+this.props.match.params.id} style={{backgroundColor:'#fc9d9a',color:'white',fontSize:'18px'}} onClick={this.onPost}>发布</Link>
                 ]}
                 >社区发布文章</NavBar>
 
