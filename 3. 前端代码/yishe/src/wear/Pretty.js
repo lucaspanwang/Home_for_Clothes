a1bb95af779de9010d21d9797a5cf8187a85f3ae
PeiYumeng <457492875@qq.com>
2020-04-21 10:12:42 +0800

    app.js & color-pick

--- 3. 前端代码/yishe/src/wear/Pretty.js
+++ 3. 前端代码/yishe/src/wear/Pretty.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { Tabs, WhiteSpace, Badge,NavBar } from 'antd-mobile';
+import { Tabs, WhiteSpace, Badge,NavBar} from 'antd-mobile';
+import {Link} from 'react-router-dom';
+import Back from '../images/返回 (1).png';
+import AlphaPicker from 'react-color'
 import './wear.css';
 //图片导入
 import pre1 from '../images/pre1.png'
@@ -30,6 +33,9 @@ export default class Pretty extends Component {
             index:0,
             index2:-1,
             index3:-1,
+            color:'',
+            color2:'',
+            count:0,//点击次数
         }
       }   
     num=(idx)=>{
@@ -47,18 +53,54 @@ export default class Pretty extends Component {
             index3:idx
         })
     }
+    lalala=()=>{//换发色的点击事件
+        document.getElementById('imgpick').style.display='block';
+        // document.getElementById('mote_3').style.display='block';
+        // document.getElementById('mote_3').style.filter=`drop-shadow(150px 0 ${this.state.color})`;
+        // document.getElementById('mote_3').style.opacity=0.9
+        console.log(this)
+        this.state.count++;//判断点击几次
+        var a = setTimeout(()=>{
+          if(this.state.count>1){//双击
+            document.getElementById('imgpick').style.display='none';
+            this.setState({
+              count:0
+            })
+          }
+        }) 
+      }
+    huance=()=>{
+        this.setState({
+          color:document.getElementById('imgpick').childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].value
+        })
+        console.log(document.getElementById('imgpick').childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].value)
+        // document.getElementById('mote_3').style.filter=`drop-shadow(150px 0 ${this.state.color})`;
+        // document.getElementById('mote_3').style.opacity=0.9
+      }
     render() {
         return (
             <div>
-              {/* 头 */}
-              <NavBar style={{backgroundColor:'#fc9d9a',color:'white'}}>穿搭</NavBar>
+            {/* 头 */}
+                <NavBar
+                    leftContent={
+                        <Link to={"/apptab/"+this.props.match.params.id}>
+                            <img src={Back} style={{ width: '30px', height: "30px" }} key="fan"/></Link>
+                    }
+                style={{backgroundColor:'rgb(252, 157, 154)'}}>穿搭</NavBar>
+
               {/* 模特脸 */}
-              <div className="m1">
+              <div className="m1" onClick={this.lalala}>
                   {/* 依次是：模特本身、发型、瞳色、眼镜 */}
                   <img src={pre1} style={{width:'100%'}}/>
                   <img src={eye[this.state.index2]} id="t2"/>
                   <img src={glasses[this.state.index3]} id="t3"/>
                   <img src={hair[this.state.index]} id="t0"/>
+              </div>
+            {/* <CompactPicker/> */}
+                          <div id="imgpick">
+                <AlphaPicker
+                style={{width:'50px'}}
+                id="imgpick2" color={this.state.color} onChange={this.huance}/>
               </div>
               <div>
                 <Tabs tabs={tabs}
@@ -70,7 +112,7 @@ export default class Pretty extends Component {
                 <div style={{height: '150px', backgroundColor: '#fff' }}>
                     {
                           hair.map((item,idx)=>(
-                            <div className="pretty1" onClick={this.num.bind(this,idx)}>
+                            <div key={idx} className="pretty1" onClick={this.num.bind(this,idx)}>
                                 <img className="pretty2" src={item} style={{width:'100%'}}/>
                             </div>
                           ))
@@ -80,7 +122,7 @@ export default class Pretty extends Component {
                     {/* 瞳色 */}
                     {
                           eye.map((item,idx)=>(
-                            <div className="pretty1" onClick={this.num2.bind(this,idx)}>
+                            <div key={'2'+idx} className="pretty1" onClick={this.num2.bind(this,idx)}>
                                 <img className="pretty3" src={item} style={{width:'100%'}}/>
                             </div>
                           ))
@@ -90,7 +132,7 @@ export default class Pretty extends Component {
                     {/* 眼镜 */}
                     {
                           glasses.map((item,idx)=>(
-                            <div className="pretty1" onClick={this.num3.bind(this,idx)}>
+                            <div key={'3'+idx}  className="pretty1" onClick={this.num3.bind(this,idx)}>
                                 <img className="pretty3" src={item} style={{width:'100%'}}/>
                             </div>
                           ))
