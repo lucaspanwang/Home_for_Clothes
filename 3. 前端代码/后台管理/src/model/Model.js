e20f9e98db282e8d62556ede6a9af65a51af21d0
PeiYumeng <457492875@qq.com>
2020-06-01 23:35:11 +0800

    add

--- 3. 前端代码/后台管理/src/model/Model.js
+++ 3. 前端代码/后台管理/src/model/Model.js
@@ -4,6 +4,8 @@ import { ImagePicker, WingBlank, SegmentedControl } from 'antd-mobile';
 import mote from '../images/mote_1.png'
 import mote2 from '../images/mote_2.png'
 import xiaoren from '../images/xiaoren.png'
+import boy from '../images/boy.gif'
+import girl from '../images/girl.gif'
 
 const data = [{
   url: mote,
@@ -13,16 +15,20 @@ const data = [{
   id: '2122',
 }];
 const data2 = [{
-  url: xiaoren,
+  url: boy,
   id: '2121',
+}, {
+  url: girl,
+  id: '2122',
 }];
 
 export default class Model extends Component {
     state = {
-      files2:data2,
+        files2:data2,
         files: data,
         multiple: false,
         ress:[],
+        ress2:[],
       }
       onChange = (files, type, index) => {
         console.log(files, type, index);
@@ -37,27 +43,58 @@ export default class Model extends Component {
         });
       }
       componentDidMount(){
-        fetch('http://47.98.163.228:8088/clo')
-        .then(res=>res.json())
-        .then(res=>{    
-          var arr=this.state.ress;
-          for(var i = 0;i<res.length;i++){
-            var name = res[i].cloSmallPic.split('/')[2]       
-            // console.log(name);
-            var str = 'http://47.98.163.228:8088/images/'+name;
-            // arr.push('http://47.98.163.228:8088/images/'+name) 
-            var obj={
-              id:i,
-              url:str,
-            }
-            arr.push(obj)
-          }
-            this.setState({
-                ress:arr
-            },function(){
-                console.log(this.state.ress)
-            })
-        })
+        var arr=[],i=0;
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/dadishan_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/chenshan_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/gezishan_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/duanxiu_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/mianfu_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/weiyi_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/yundongyi_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/dayitao_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/xizhuangwaitao_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/maoxianwaitao_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/xizhuangwaitao_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/yundongwaitao_s.png'}
+        arr.push(obj)
+        this.setState({
+          ress:arr
+       })
+       arr=[];
+       var obj={id:i++,url:'http://47.98.163.228:3001/aaa/beidaiku_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/duanku_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/zhitongku_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/yundongku_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/xizhuangku_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/diaodaiqun_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/gongzuoqun_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/suihuaqun_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/yundongqun_s.png'}
+        arr.push(obj)
+        var obj={id:i++,url:'http://47.98.163.228:3001/aaa/liqun_s.png'}
+        arr.push(obj)
+        this.setState({
+          ress2:arr
+       })
     }
     render() {
         return (
@@ -65,24 +102,32 @@ export default class Model extends Component {
                 <div id="page-inner">
                 <WingBlank>
                 <p>模特</p>
-                <ImagePicker style={{width:'70%'}}
+                <ImagePicker style={{width:'40%'}}
                 files={this.state.files}
                 onChange={this.onChange}
                 onImageClick={(index, fs) => console.log(index, fs)}
-                selectable={this.state.files.length < 7}
+                selectable={this.state.files.length < 2}
                 multiple={this.state.multiple}
                 />
                 <p>小人模型</p>
-                <ImagePicker style={{width:'70%'}}
+                <ImagePicker style={{width:'40%'}}
                 files={this.state.files2}
                 onChange={this.onChange}
                 onImageClick={(index, fs) => console.log(index, fs)}
-                selectable={this.state.files2.length < 7}
+                selectable={this.state.files2.length < 2}
                 multiple={this.state.multiple}
                 />
                 <p>衣服模型</p>
-                <ImagePicker style={{width:'70%'}}
+                <ImagePicker style={{width:'40%',float:'left'}}
                 files={this.state.ress}
+                onChange={this.onChange}
+                onImageClick={(index, fs) => console.log(index, fs)}
+                selectable={this.state.ress.length < 7}
+                multiple={this.state.multiple}
+                />
+                {/* <p>衣服模型</p> */}
+                <ImagePicker style={{width:'40%',float:'left'}}
+                files={this.state.ress2}
                 onChange={this.onChange}
                 onImageClick={(index, fs) => console.log(index, fs)}
                 selectable={this.state.ress.length < 7}
