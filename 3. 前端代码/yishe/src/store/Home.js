e5b5869538be155c63a1732d7cfbebe8cba6b902
zhangzhuozhang <2028251635@qq.com>
2020-06-07 21:21:44 +0800

    store

--- 3. 前端代码/yishe/src/store/Home.js
+++ 3. 前端代码/yishe/src/store/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Accordion, List } from 'antd-mobile';
 import {Link} from 'react-router-dom';
 import { NavBar, Icon } from 'antd-mobile';
 import Back from '../images/fanhui_1.png';
@@ -8,7 +9,9 @@ export default class Home extends Component {
         super();
         this.state={
             url:'http://47.98.163.228:8084/home',
-            picture:[],//记录图片路径
+            pictureSummer:[],//记录图片路径
+            pictureString:[],
+            pictureWinter:[],
             num:-1,
             result:''//标记那个是红标
         }
@@ -33,18 +36,7 @@ export default class Home extends Component {
             body:JSON.stringify({nage:i,weizhi:'家',userId:this.props.match.params.id})
         })
     }
-    // componentWillMount(){
-    //     fetch("http://47.98.163.228:8084/userid", {
-    //     method: 'post', 
-    //     "Access-Control-Allow-Origin" : "*",
-    //     "Access-Control-Allow-Credentials" : true,
-    //     credentials: 'include',
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //     },
-    //     body:JSON.stringify({userId:this.props.match.params.id,zhengli:'家'}) 
-    //   });
-    // }
+    
     componentDidMount(){
         //图片显示
         fetch('http://47.98.163.228:3003/picture', {
@@ -58,11 +50,14 @@ export default class Home extends Component {
           userId:this.props.match.params.id,
           whereId:"1"
         })
-      }).then((res)=>res.json())
+      })
+      .then((res)=>res.json())
       .then((res)=>{
-        //   console.log('图片路径接收：'+JSON.stringify(res))
+        //   console.log('新的图片路径：'+res.picString)
         this.setState({
-            picture:res
+            pictureString:res.picString,
+            pictureSummer:res.picSummer,
+            pictureWinter:res.picWinter
         })
       })
       //那个是红标
@@ -72,72 +67,6 @@ export default class Home extends Component {
             })
             localStorage.setItem('搜索', '')
         }
-    //   fetch('http://47.98.163.228:3003/home')
-    //     .then(res=>res.json())
-    //     .then(res=>{
-    //         console.log('home',res)
-    //         this.setState({
-    //             result:res
-    //         })
-    //     })
-    //   console.log(this.props.match.params.id)
-    //     fetch("http://47.98.163.228:8084/userid", {
-    //     method: 'post', 
-    //     "Access-Control-Allow-Origin" : "*",
-    //     "Access-Control-Allow-Credentials" : true,
-    //     credentials: 'include',
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //     },
-    //     body:JSON.stringify({userId:this.props.match.params.id,zhengli:'家'}) 
-    //   });
-        // console.log(this.props.match.params.id);//获取用户id
-        // fetch('http://47.98.163.228:3003/home')
-        // .then(res=>res.json())
-        // .then(res=>{
-        //     console.log('图片地址：'+res)
-        //     this.setState({
-        //         picture:res
-        //     })
-        //     console.log(this.state.picture)
-        // });
-
-        // var n=localStorage.getItem('count');
-        // localStorage.setItem('count',n+1);
-        // console.log('第几个'+n)
-        // var nn=localStorage.getItem('count');
-        // if(nn==1){
-        //     this.setState({
-        //         num:localStorage.getItem('zlx_num')-1
-        //     },function(){
-        //         localStorage.setItem('zlx_num',-1)
-        //     })
-        //     console.log('获取的num'+this.state.num)
-        // }else{
-        //     this.setState({
-        //         num:-1
-        //     })
-        // }
-        
-        
-    //   fetch('http://47.98.163.228:3001/pp2')
-    //     .then(res=>res.json())
-    //     .then(res=>{
-    //         console.log(res);
-    //         console.log('????'+res.msg)
-    //         var n = localStorage.getItem('count')
-    //         localStorage.setItem('count', n+1)
-    //         var nn = localStorage.getItem('count')
-    //         if(nn==1){
-    //             this.setState({
-    //                 num:res.msg-1
-    //             })
-    //         }else{
-    //             this.setState({
-    //                 num:-1
-    //             })
-    //         }
-        // });
     }
     render() {
         return (
@@ -151,11 +80,117 @@ export default class Home extends Component {
                     >
                     {localStorage.getItem('jia')?localStorage.getItem('jia'):'家'}
                     </NavBar>
-                <div style={{position:'relative'}}>
+                <div style={{ marginTop: 10, marginBottom: 10 }}>
+                    <Accordion defaultActiveKey="0" className="my-accordion" onChange={this.onChange}>
+                        <Accordion.Panel header="夏季">
+                            <div style={{ position: 'relative' }}>
+                                {
+                                    this.state.pictureSummer.map((item, i) => {
+                                        if (i == this.state.num) {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '1px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else if (item == this.state.result) {
+
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '2px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                }
+                            </div>
+                            
+                        </Accordion.Panel>
+                        <Accordion.Panel header="春秋" className="pad">
+                            <div style={{ position: 'relative' }}>
+                                {
+                                    this.state.pictureString.map((item, i) => {
+                                        if (i == this.state.num) {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '1px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else if (item == this.state.result) {
+
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '2px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                }
+                            </div>
+                        </Accordion.Panel>
+                        <Accordion.Panel header="冬季" className="pad">
+                        <div style={{ position: 'relative' }}>
+                                {
+                                    this.state.pictureWinter.map((item, i) => {
+                                        if (i == this.state.num) {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '1px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else if (item == this.state.result) {
+
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px', border: '2px solid red' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div key={i} style={{ display: 'inlinbe-block', position: 'relative', width: '32%', height: "120px", margin: '2px', float: 'left' }}>
+                                                    <img src={`http://47.98.163.228:3004/${item}`} style={{ width: '100%', height: '120px' }} />
+                                                    <span style={{ position: "absolute", color: 'red', right: '5px', top: '-3px' }} onClick={this.deleteItem.bind(this, i)}>x</span>
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                }
+                            </div>
+                        </Accordion.Panel>
+                    </Accordion>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+//原来显示图片
+{/* <div style={{position:'relative'}}>
                     {
                     this.state.picture.map((item,i)=>{
-                        console.log('item',item)
-                        console.log('搜索结果',this.state.result)
                         if(i==this.state.num){
                             return(
                                 <div key={i} style={{display:'inlinbe-block',position:'relative',width:'32%',height:"120px",margin:'2px',float:'left'}}>
@@ -181,8 +216,4 @@ export default class Home extends Component {
                         }
                     })
                     }
-                </div>
-            </div>
-        )
-    }
-}
+                </div> */}
